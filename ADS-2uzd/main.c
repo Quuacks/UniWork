1546cd77aeb0ed090583fc63d886e339d25072b4
Quuacks <staneviciusarmantas@gmail.com>
2026-03-29 15:07:38 +0300

    Finished program

--- ADS-2uzd/main.c
+++ ADS-2uzd/main.c
@@ -3,77 +3,78 @@
 #include <string.h>
 typedef struct Daiktas
 {
-    float svoris;
-    float kaina;
+    int svoris;
+    int kaina;
 }Daiktas;
 
-void readFromFile(char *fileName, Daiktas **daiktai, int *n);
-void readFromFileNoN(char *fileName, Daiktas **daiktai, int *n);
-void sortByPrice(Daiktas *daiktai, int n);
+void readFromFile(char *fileName, Daiktas **daiktai, int *n, int *S);
+void readFromFileNoN(char *fileName, Daiktas **daiktai, int *n, int *S);
+int findBestForS(Daiktas *daiktai, int n, int S);
 
 int main(){
-    int n;
+    int n, S;
     Daiktas *daiktai = NULL;
-    char *fileName = "test.txt";
+    char fileName[100];
 
-    readFromFile(fileName, &daiktai, &n);
-    sortByPrice(daiktai, n);
+    printf("File name: ");
+    scanf("%99s", fileName);
 
-    printf("n = %d\n", n);
+    readFromFile(fileName, &daiktai, &n, &S);
 
-    for(int i = 0; i < n; i++){
-        printf("%d. Svoris: %.2f, Kaina: %.2f\n", i, daiktai[i].svoris, daiktai[i].kaina);
-    }
+    int maxKaina = findBestForS(daiktai, n, S);
+
+    printf("Max kaina: %d", maxKaina);
+
+    free(daiktai);
 }
-void readFromFile(char *fileName, Daiktas **daiktai, int *n){
+void readFromFile(char *fileName, Daiktas **daiktai, int *n, int *S){
     FILE *fptr = fopen(fileName, "r");
 
     if(!fptr)
         return;
 
-    fscanf(fptr, "%d", n);
+    fscanf(fptr, "%d %d", n, S);
     *daiktai = calloc(*n, sizeof(Daiktas));
     for(int i = 0; i < *n; ++i){
-        fscanf(fptr, "%f %f", &(*daiktai)[i].svoris, &(*daiktai)[i].kaina);
+        fscanf(fptr, "%d %d", &(*daiktai)[i].svoris, &(*daiktai)[i].kaina);
     }
 
     fclose(fptr);
 }
-void readFromFileNoN(char *fileName, Daiktas **daiktai, int *n){
+void readFromFileNoN(char *fileName, Daiktas **daiktai, int *n, int *S){
     FILE *fptr = fopen(fileName, "r");
 
     if(!fptr)
         return;
 
+    fscanf(fptr, "%d", S);
+
     int size = 10;
     *daiktai = calloc(size, sizeof(Daiktas));
     int i = 0;
-    while(fscanf(fptr, "%f %f", &(*daiktai)[i].svoris, &(*daiktai)[i].kaina) == 2){
+    while(fscanf(fptr, "%d %d", &(*daiktai)[i].svoris, &(*daiktai)[i].kaina) == 2){
         i++;
         if(i >= size){
             size += 10;
-            calloc(size, sizeof(Daiktas));
-            if(daiktai == NULL)
-                return;
+            (*daiktai) = realloc(*daiktai, size * sizeof(Daiktas));
+            
         }
     }
     *n = i;
 }
-void sortByPrice(Daiktas *daiktai, int n){
-    for(int i = 0; i < n; ++i){
-        for(int j = i; j < n; ++j){
-            if(daiktai[i].kaina < daiktai[j].kaina){
-                Daiktas tmp = daiktai[i];
-                daiktai[i] = daiktai[j];
-                daiktai[j] = tmp;
-            }
-            else if(daiktai[i].kaina == daiktai[j].kaina){
-                if(daiktai[i].svoris > daiktai[j].svoris){
-                    Daiktas tmp = daiktai[i];
-                    daiktai[i] = daiktai[j];
-                    daiktai[j] = tmp;
-                }
+int findBestForS(Daiktas *daiktai, int n, int S){
+    int *dp = calloc(S + 1, sizeof(int));
+
+    for(int i = 0; i < n; i++){
+        for(int w = S; w >= daiktai[i].svoris; w--){
+            int newVal = dp[w - daiktai[i].svoris] + daiktai[i].kaina;
+            if(newVal > dp[w]){
+                dp[w] = newVal;
             }
         }
     }
+
+    int result = dp[S];
+    free(dp);
+    return result;
 }
